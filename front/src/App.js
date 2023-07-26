6242d6c86544481cb3fdcc3f97b66c57039b4570
StevenJossueAchig <stevenjossue5@gmail.com>
2023-07-26 10:20:00 -0500

    Cv

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
-<<<<<<< HEAD
 import Inicio from './components/Inicio';
 import { AuthProvider } from './contexts/AuthContext';
-
-const router = createBrowserRouter([
-  { path: '/', element: <Login/> },
-  { path: '/Inicio', element: <Inicio/> },
-]);
-
-function App() {
-  
-  return (<AuthProvider><RouterProvider router = {router} />
-  </AuthProvider> );
-  
-=======
 import CV from './pages/cv';
 
 function App() {
@@ -28,7 +15,6 @@ function App() {
     </Routes>
   </BrowserRouter>
   );
->>>>>>> 1200a1c7296c2c093f9bdf0abdb3fca9cbf09164
 }
 
 export default App;
--- front/src/pages/cv.js
+++ front/src/pages/cv.js
@@ -1,68 +1,142 @@
-import React from 'react'
+import React, { useState } from 'react';
 import "../styles/cv.css"
 
-export default function CV() {
+const Inicio = () => {
+  const [idDocente, setIdDocente] = useState('');
+  const [tipoDocumento, setTipoDocumento] = useState('');
+  const [numeroDocumento, setNumeroDocumento] = useState('');
+  const [genero, setGenero] = useState('');
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
+  const [ciudadResidencia, setCiudadResidencia] = useState('');
+  const [direccion, setDireccion] = useState('');
+  const [correoElectronico, setCorreoElectronico] = useState('');
+  const [numeroTelefono, setNumeroTelefono] = useState('');
+  const [fechaNacimiento, setFechaNacimiento] = useState('');
 
+  const handleSaveChanges = () => {
+    // Aquí puedes enviar los datos a un servidor o realizar alguna acción con ellos.
+    // Por ahora, solo mostraremos los datos en la consola.
+    console.log('ID Docente:', idDocente);
+    console.log('Tipo de documento:', tipoDocumento);
+    console.log('Número de documento:', numeroDocumento);
+    console.log('Género:', genero);
+    console.log('Nombre:', nombre);
+    console.log('Apellido:', apellido);
+    console.log('Ciudad de residencia:', ciudadResidencia);
+    console.log('Dirección:', direccion);
+    console.log('Correo electrónico:', correoElectronico);
+    console.log('Número de teléfono:', numeroTelefono);
+    console.log('Fecha de nacimiento:', fechaNacimiento);
+  };
 
-    return (
-        <section className='cv'>
-            <div className="form-box1">
-                <div className="form-value1">
-                    <form action="" id='formulario1'>
+  return (
+    <div>
+      <h2>Curriculum Vitae</h2>
+      <form>
+        <div>
+          <label htmlFor="idDocente">ID Docente:</label>
+          <input
+            type="text"
+            id="idDocente"
+            value={idDocente}
+            onChange={(e) => setIdDocente(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="tipoDocumento">Tipo de documento:</label>
+          <input
+            type="text"
+            id="tipoDocumento"
+            value={tipoDocumento}
+            onChange={(e) => setTipoDocumento(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="numeroDocumento">Número de documento:</label>
+          <input
+            type="text"
+            id="numeroDocumento"
+            value={numeroDocumento}
+            onChange={(e) => setNumeroDocumento(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="genero">Género:</label>
+          <input
+            type="text"
+            id="genero"
+            value={genero}
+            onChange={(e) => setGenero(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="nombre">Nombre:</label>
+          <input
+            type="text"
+            id="nombre"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="apellido">Apellido:</label>
+          <input
+            type="text"
+            id="apellido"
+            value={apellido}
+            onChange={(e) => setApellido(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="ciudadResidencia">Ciudad de residencia:</label>
+          <input
+            type="text"
+            id="ciudadResidencia"
+            value={ciudadResidencia}
+            onChange={(e) => setCiudadResidencia(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="direccion">Dirección:</label>
+          <input
+            type="text"
+            id="direccion"
+            value={direccion}
+            onChange={(e) => setDireccion(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="correoElectronico">Correo electrónico:</label>
+          <input
+            type="email"
+            id="correoElectronico"
+            value={correoElectronico}
+            onChange={(e) => setCorreoElectronico(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="numeroTelefono">Número de teléfono:</label>
+          <input
+            type="tel"
+            id="numeroTelefono"
+            value={numeroTelefono}
+            onChange={(e) => setNumeroTelefono(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="fechaNacimiento">Fecha de nacimiento:</label>
+          <input
+            type="date"
+            id="fechaNacimiento"
+            value={fechaNacimiento}
+            onChange={(e) => setFechaNacimiento(e.target.value)}
+          />
+        </div>
+      </form>
+      <button onClick={handleSaveChanges}>Guardar cambios</button>
+    </div>
+  );
+};
 
-                        <h2>Datos Personales</h2>
-
-                        <div className="inputbox1">
-                            <ion-icon name="lock-closed-outline"></ion-icon>
-                            <input type="text" required />
-                            <label htmlFor="">Nombre</label>
-                        </div>
-
-                        <div className="inputbox1">
-                            <ion-icon name="lock-closed-outline"></ion-icon>
-                            <input type="text" required />
-                            <label htmlFor="">Nacionalidad</label>
-                        </div>
-                        
-                        <div className="inputboxsecondname1">
-                            <ion-icon name="lock-closed-outline"></ion-icon>
-                            <input type="text" required />
-                            <label htmlFor="">Apellido</label>
-                        </div>
-
-                        <div className="inputboxCI1">
-                            <ion-icon name="lock-closed-outline"></ion-icon>
-                            <input type="text" required />
-                            <label htmlFor="">CI</label>
-                        </div>
-
-                        <div className="selectbox1">
-                            <ion-icon name="lock-closed-outline"></ion-icon>
-                            <label htmlFor="sexo">Sexo</label>
-                            <select id="sexo" required>
-                                <option value="">Selecciona una opción</option>
-                                <option value="male">Masculino</option>
-                                <option value="female">Femenino</option>
-                            </select>
-                        </div>
-
-                        <div className="selectbox1">
-                            <ion-icon name="lock-closed-outline"></ion-icon>
-                            <label htmlFor="civilstate">Estado Civil</label>
-                            <select id="civilstate" required>
-                                <option value="">Selecciona una opción</option>
-                                <option value="single">Soltero</option>
-                                <option value="married">Casado</option>
-                                <option value="divorce">Divorciado</option>
-                                <option value="freeunion">Union Libre</option>
-                            </select>
-                        </div>
-                        <button>Editar</button>
-
-                    </form>
-
-                </div>
-            </div>
-        </section>
-    )
-}
+export default Inicio;
