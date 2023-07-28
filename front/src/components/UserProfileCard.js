f4a415f12a46574e6b528b018985f577ffb43782
Genesis-Calapaqui <gicalapaqui@espe.edu.ec>
2023-07-27 23:06:12 -0500

    Update UserProfileCard.js
    Actualizar fotografía

--- front/src/components/UserProfileCard.js
+++ front/src/components/UserProfileCard.js
@@ -1,5 +1,5 @@
 // UserProfileCard.js
-import React from 'react';
+import {React,useState,useContext,useLayoutEffect} from 'react';
 import { makeStyles } from '@mui/styles';
 import Avatar from '@mui/material/Avatar';
 import TextField from '@mui/material/TextField';
@@ -7,6 +7,10 @@ import Autocomplete from '@mui/material/Autocomplete';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import Typography from '@mui/material/Typography';
+import axios from 'axios';
+import { AuthContext } from '../contexts/AuthContext';
+
+const imagenes = require.context('../../../backend_API/src/images',true);
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -42,31 +46,66 @@ const useStyles = makeStyles((theme) => ({
 
 const UserProfileCard = ({ user, countries, roles }) => {
   const classes = useStyles();
-
+  const [file, setFile] =useState();
+  const [artistasList, setArtistasList] = useState([]);
+  const { userEmail} = useContext(AuthContext);
+  const handleFile = (e) => {
+    setFile(e.target.files[0])
+  }
+  const handleUpload = () => {
+    const formdata = new FormData();
+    formdata.append('image', file);
+    const id_docente=userEmail;
+    axios.post(`http://localhost:8000/upload/${id_docente}`,formdata)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+  }
+  const [docentes, setDocentes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useLayoutEffect(()=> {
+    const id_docente=userEmail;
+    console.log(id_docente);
+    axios.get(`http://localhost:8000/docente/${id_docente}`).then((response) => {
+      setDocentes(response.data);
+      setIsLoading(false);
+      console.log(response.data);
+      
+    });
+    
+  }, []);
   return (
+
     <div className={classes.root}>
-        
+        {isLoading ? (
+        <div>Cargando datos...</div>
+      ) : (
         <div className={classes.avatarContainer}>
-            <Avatar className={classes.avatar} alt={user.name} src={user.avatarUrl} />
-            <Typography variant="h6" align="center">
-            {user.name}
-            </Typography>
+        <Avatar className={classes.avatar} alt={user.name} src={imagenes(`./${docentes[0].fotografia}`)} />
+        <input type="file" onChange={handleFile}/>
+        <button onClick={handleUpload}>Actualizar</button>
+          <Typography variant="h6" align="center">
+          {docentes[0].nombre}
+          </Typography>
         </div> 
 
-      <div className={classes.formContainer}>
+      )}
+      {isLoading ? (
+        <div>Cargando datos...</div>
+      ) : (
+        <div className={classes.formContainer}>
         <TextField
           label="Name"
           variant="outlined"
           fullWidth
           margin="normal"
-          defaultValue={user.name}
+          defaultValue={docentes[0].nombre}
         />
         <TextField
           label="Email"
           variant="outlined"
           fullWidth
           margin="normal"
-          defaultValue={user.email}
+          defaultValue={docentes[0].correo_electronico}
         />
         {/* Add more personal data fields here */}
         <div className={classes.comboContainer}>
@@ -92,7 +131,12 @@ const UserProfileCard = ({ user, countries, roles }) => {
           </Select>
         </div>
       </div>
+
+      )}
+
+
     </div>
+
   );
 };
 
